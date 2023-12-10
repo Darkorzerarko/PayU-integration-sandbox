f1f084be457a912ebd780e513d825d354e21af32
Darkorzerarko <rz.dariusz@gmail.com>
2023-12-10 15:25:18 +0100

    Fetch test v2.2

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,30 @@
 "use client"
 import styles from './page.module.scss'
-// import { useState } from 'react'
 
 export default function Home() {
 
-  // const [response, setResponse] = useState('');
+  const url = 'https://secure.snd.payu.com/pl/standard/user/oauth/authorize';
+  const data = new URLSearchParams();
+  data.append('grant_type', 'client_credentials');
+  data.append('client_id', '460718');
+  data.append('client_secret', '22f4175da9f0f72bcce976dd8bd7504f');
 
   const  handleClick = ()=>{
-    fetch("https://secure.snd.payu.com/pl/standard/user/oauth/authorize", {
-        method: "POST",
-        // mode: "no-cors",
-        body: JSON.stringify({
-          grant_type: "client_credentials",
-          client_id: '474204',
-          client_secret: "ea9084d89e60ff23e2913fe5afcdde81"
-        }),
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-        },
-      }
-    )
-    .catch(
-      console.log("nie działa")
-    )
-    .then(e => {
-      console.log(e)
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: data,
     })
-    // .then((response)=>response.json())
-    // .then((response)=>console.log(response))
-    // alert("click")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
   }
 
+
+  // handleClick()
 
   return (
     <main className={styles.main}>
